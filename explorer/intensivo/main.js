76fe67f12b7662f2899b7b6b437c21ab76761664
Pedro Sousa <pedrosousa@unipam.edu.br>
2024-03-13 14:10:46 -0300

    intensivo js

---
+++ explorer/intensivo/main.js
@@ -0,0 +1,92 @@
+/*
+1- Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+alert("Hello World!")
+
+2- Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+let x = 1
+let y = 2
+let sum = x + y
+
+console.log(sum)
+
+3-Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+let number = 0
+let variable = "Pedro"
+if (typeof variable == typeof number){
+    alert("É um número")
+}else{
+    alert("Nao é um número")
+}
+
+**Condicional ternário
+
+let number = 0
+let variable = 8
+
+typeof variable == typeof number ? alert("é número") : alert("Não é número")
+
+4-Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+let isString = typeof "Pedro"
+let teacherName = "Mayk"
+
+if(typeof teacherName == isString){
+    alert("É string")
+}else{
+    alert("Não é string")
+}
+
+5-Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+let isBoolean = typeof true
+let trueOrFalse = false
+
+if(typeof trueOrFalse == isBoolean){
+    alert("É booleano")
+}else{
+    alert("Não é booleano")
+}
+
+6- Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+const x = 7
+const y = 4
+
+let sub = x - y
+
+alert(sub)
+
+7- Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+const x = 7
+const y = 4
+
+let mult = x * y
+
+alert(mult)
+
+8- Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+const x = 8
+const y = 4
+
+let div = x / y
+
+alert(div)
+
+9- Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+const isEven = 5
+
+isEven % 2 == 0 ? alert("É par") : alert("Não é par")
+
+10- Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+const isOdd = 9
+
+isOdd % 2 == 1 ? alert("É ímpar") : alert("Nao é ímpar")
+
+*/
